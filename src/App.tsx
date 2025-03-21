88179c46b8c610f71e010de6c349c444b727e57d
dracekmracek <lukeadam3@gmail.com>
2025-03-21 21:28:36 +0100

    TEST publikace

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/powder-coat-pro">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
